d151c68f941f450fab3322bb3cb20c3c9c5969ab
7beunseo <kimes0403@gmail.com>
2025-02-10 11:22:58 +0900

    🎉 Feature: 프로젝트 생성

---
+++ src/main/java/com/backend/payring/PayringApplication.java
@@ -0,0 +1,13 @@
+package com.backend.payring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/payring/PayringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.payring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

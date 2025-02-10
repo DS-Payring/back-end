3d23dccd9ae11988764c07dbc34661614c642c76
HeewonJo <heewonjo.me@gmail.com>
2025-02-10 13:37:00 +0900

    ⚙️ Setting: UserEntity 추가

---
+++ src/main/java/com/backend/payring/entity/UserEntity.java
@@ -0,0 +1,24 @@
+package com.backend.payring.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}

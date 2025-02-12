9fe11bece6659337d74ab2fcadcee2a452030138
7beunseo <kimes0403@gmail.com>
2025-02-13 02:43:20 +0900

    ✨ Feature: 인증코드 메일 html 템플릿 적용

--- src/main/java/com/backend/payring/service/EmailServiceImpl.java
+++ src/main/java/com/backend/payring/service/EmailServiceImpl.java
@@ -1,9 +1,14 @@
 package com.backend.payring.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
 
 import java.util.Random;
 
@@ -11,17 +16,25 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class EmailServiceImpl {
     private final JavaMailSender mailSender;
+    private final TemplateEngine templateEngine;
 
-    public String generateVerificationCode(String email) {
+    public String generateVerificationCode() {
         Random random = new Random();
         return String.format("%04d", random.nextInt(10000));
     }
 
-    public void sendVerificationCode(String email, String verificationCode) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject("Payring 이메일 인증 번호");
-        message.setText("인증 코드: " + verificationCode);
+    public void sendVerificationCode(String email, String verificationCode) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        helper.setTo(email);
+        helper.setSubject("Payring 이메일 인증 번호");
+
+        Context context = new Context();
+        context.setVariable("verificationCode", verificationCode);
+        String htmlContent = templateEngine.process("code", context);
+
+        helper.setText(htmlContent, true);
         mailSender.send(message);
     }
 }

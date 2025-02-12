a08ce25d7971ae72750687a3bc8c840485d15078
7beunseo <kimes0403@gmail.com>
2025-02-13 06:40:46 +0900

    🔨 Refactor: Swagger JWT 설정

--- src/main/java/com/backend/payring/config/SwaggerConfig.java
+++ src/main/java/com/backend/payring/config/SwaggerConfig.java
@@ -9,11 +9,32 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration  // Spring 설정 클래스로 등록
 public class SwaggerConfig {
 
+
     @Bean
     public OpenAPI PAYRING_API() {
+
+        Server testServer = new Server();
+        testServer.setUrl("http://localhost:8080/");
+
+        Server deployServer = new Server();
+        deployServer.setUrl("https://carely-backend.site/");
+
+
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .description("토큰값을 입력하여 인증을 활성화할 수 있습니다.")
+                .bearerFormat("JWT")
+        );
+
         Info info = new Info()
                 .title("PAYRING API")
                 .description("PAYRING API 명세서")
@@ -21,7 +42,9 @@ public class SwaggerConfig {
 
 
         return new OpenAPI()
-                .addServersItem(new Server().url("/"))
-                .info(info);
+                .components(components)
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .servers(List.of(deployServer, testServer));
     }
 }

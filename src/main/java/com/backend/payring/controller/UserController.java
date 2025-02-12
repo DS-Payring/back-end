0ecabe0a05b1ef4ed448272efb2cf4e2afce102b
7beunseo <kimes0403@gmail.com>
2025-02-13 06:19:38 +0900

    📝 Docs: UserController Swagger 문서 작성

--- src/main/java/com/backend/payring/controller/UserController.java
+++ src/main/java/com/backend/payring/controller/UserController.java
@@ -8,6 +8,7 @@ import com.backend.payring.entity.UserEntity;
 import com.backend.payring.exception.UserException;
 import com.backend.payring.repository.UserRepository;
 import com.backend.payring.service.UserServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -30,6 +31,10 @@ public class UserController {
     private final UserServiceImpl userServiceImpl;
     private final UserRepository userRepository;
 
+    @Operation(
+            summary = "이메일 인증번호 요청 API",
+            description = "이메일 인증번호 코드를 요청합니다. 이때 유저가 생성됩니다."
+    )
     @PostMapping("/email/verify/send")
     public ResponseEntity<?> sendVerificationCode(@RequestParam String email) throws MessagingException {
         userServiceImpl.sendVerificationCode(email);
@@ -38,6 +43,11 @@ public class UserController {
                 .body(new ResponseDTO<>(ResponseCode.SUCCESS_SEND_EMAIL, null));
     }
 
+
+    @Operation(
+            summary = "인증번호 인증 API",
+            description = "인증번호를 입력하여 이메일 인증을 완료합니다."
+    )
     @PostMapping("/email/verify")
     public ResponseEntity<?> verifyEmail(@RequestBody @Valid UserDTO .EmailVerificationReq req) {
         userServiceImpl.verifyEmail(req);
@@ -46,6 +56,10 @@ public class UserController {
                 .body(new ResponseDTO<>(ResponseCode.SUCCESS_VERIFY_EMAIL, null));
     }
 
+    @Operation(
+            summary = "회원가입 API",
+            description = "회원가입을 진행합니다."
+    )
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody @Valid UserDTO.SignUpReq req) {
 //        if (userServiceImpl.existsByEmail(req.getEmail())) {
@@ -57,6 +71,10 @@ public class UserController {
                 .body(new ResponseDTO<>(ResponseCode.SUCCESS_SIGNUP, res));
     }
 
+    @Operation(
+            summary = "로그인 API",
+            description = "로그인하여 토큰을 얻습니다."
+    )
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid UserDTO.LoginReq req) {
         UserDTO.Res res = userServiceImpl.login(req);
@@ -65,6 +83,10 @@ public class UserController {
                 .body(new ResponseDTO<>(ResponseCode.SUCCESS_LOGIN, res));
     }
 
+    @Operation(
+            summary = "(테스트용) 유저id 확인 API",
+            description = "유저아이디가 떠야 합니다."
+    )
     @GetMapping("/profile")
     public String getUserProfile(@AuthenticationPrincipal UserEntity user) {
         if (user == null) {
